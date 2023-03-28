04cbe0eb58ca6460deb665f0a905f455f45eb1ee
Matthew Brabham <matthew.brabham@oregonstate.edu>
2023-03-28 11:58:10 -0700

    Add custom process plugin to set the new FID of the image. Update migrations for 2 column.

--- modules/paragraphs_to_layout_builder/src/Plugin/migrate/process/EntityBackground.php
+++ modules/paragraphs_to_layout_builder/src/Plugin/migrate/process/EntityBackground.php
@@ -49,8 +49,7 @@ class EntityBackground extends ProcessPluginBase implements ContainerFactoryPlug
    * {@inheritDoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition, $container->get('migrate.lookup')
-    );
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('migrate.lookup'));
   }
 
   /**
