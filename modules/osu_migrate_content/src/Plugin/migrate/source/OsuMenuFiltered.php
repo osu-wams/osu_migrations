0c00221b9b298c30cab6a2730cf3c7eee319edcc
Matthew Brabham <matthew.brabham@oregonstate.edu>
2023-04-10 16:44:29 -0700

    Migrate Menus

---
+++ modules/osu_migrate_content/src/Plugin/migrate/source/OsuMenuFiltered.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\osu_migrate_content\Plugin\migrate\source;
+
+use Drupal\system\Plugin\migrate\source\Menu;
+
+/**
+ *
+ * @MigrateSource(
+ *   id = "menu_filtered",
+ *   source_module = "menu"
+ * )
+ */
+class OsuMenuFiltered extends Menu {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function query() {
+    // Filter out book-toc menus as we will migrate them differently.
+    $query = parent::query();
+    $query->condition('m.menu_name', 'book-toc-%', 'NOT LIKE');
+    return $query;
+  }
+
+}
