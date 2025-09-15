1d4379d63538e195caa6ca4c6edba4aa036db019
Matthew Brabham <matthew.brabham@oregonstate.edu>
2025-09-15 15:44:27 -0700

    Add ShURLy Flood migration plugins
    Implemented migration source, process, and destination plugins for migrating ShURLy Flood data from Drupal 7. Added corresponding YAML configuration and ensured plugin compatibility with PHP 8.1+.

---
+++ modules/osu_migrations_shurly/src/Plugin/migrate/destination/OsuMigrationsShurlyFlood.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\osu_migrations_shurly\Plugin\migrate\destination;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\Attribute\MigrateDestination;
+use Drupal\migrate\Plugin\migrate\destination\DestinationBase;
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a ShURLy destination plugin for Flood.
+ */
+#[MigrateDestination(
+  id: 'shurly_flood'
+)]
+class OsuMigrationsShurlyFlood extends DestinationBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  private Connection $database;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, Connection $database) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+    $this->database = $database;
+    $this->supportsRollback = TRUE;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, ?MigrationInterface $migration = NULL): OsuMigrationsShurlyFlood|static {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $migration,
+      $container->get('database')
+    );
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getIds(): array {
+    return ['fid' => ['type' => 'integer']];
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function import(Row $row, array $old_destination_id_values = []): bool|array {
+    $record = [];
+    $record['fid'] = $row->getSourceProperty('fid');
+    $record['event'] = $row->getSourceProperty('event');
+    $record['identifier'] = $row->getSourceProperty('identifier');
+    $record['timestamp'] = $row->getSourceProperty('timestamp');
+    $record['expiration'] = $row->getSourceProperty('expiration');
+    return [
+      $this->database->insert('shurly_flood')
+        ->fields($record)
+        ->execute(),
+    ];
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function fields(): array {
+    return [
+      'fid' => $this->t('The flood ID.'),
+      'event' => $this->t('Name of the event.'),
+      'identifier' => $this->t('Identifier of the visitor.'),
+      'timestamp' => $this->t('The timestamp of the event.'),
+      'expiration' => $this->t('The expiration timestamp of the event.'),
+    ];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function rollback(array $destination_identifier): void {
+    $this->database->delete('shurly_flood')
+      ->condition('fid', $destination_identifier['fid'])
+      ->execute();
+  }
+
+}
---
+++ modules/osu_migrations_shurly/src/Plugin/migrate/source/OsuMigrationsShurlyFlood.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\osu_migrations_shurly\Plugin\migrate\source;
+
+use Drupal\Core\Database\Query\SelectInterface;
+use Drupal\migrate\Annotation\MigrateSource;
+use Drupal\migrate_drupal\Plugin\migrate\source\DrupalSqlBase;
+
+/**
+ * Migrate ShURLy API keys.
+ *
+ * @MigrateSource(
+ *   id = "d7_shurly_flood",
+ *   source_module = "shurly"
+ * )
+ */
+class OsuMigrationsShurlyFlood extends DrupalSqlBase {
+
+  /**
+   * {@iheritDoc}
+   */
+  public function getIds(): array {
+    return [
+      'fid' => [
+        'type' => 'integer',
+      ],
+    ];
+  }
+
+  public function query(): SelectInterface {
+    $query = $this->select('shurly_flood', 'shurly_flood');
+    $query->fields('shurly_flood', [
+      'fid',
+      'event',
+      'identifier',
+      'timestamp',
+      'expiration',
+    ]);
+    $query->distinct();
+    return $query;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function fields(): array {
+    return [
+      'fid' => $this->t('The flood ID.'),
+      'event' => $this->t('Name of the event.'),
+      'identifier' => $this->t('Identifier of the visitor.'),
+      'timestamp' => $this->t('The timestamp of the event.'),
+      'expiration' => $this->t('The expiration timestamp of the event.'),
+    ];
+  }
+
+}
